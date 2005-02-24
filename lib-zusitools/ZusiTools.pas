6c08e245578d7a691be0b0fb84ad0b568c2a4669
danny243 <danny243>
2005-02-24 18:17:31 +0000

    Funktion SendKeyToZusi hinzugefügt

--- lib-zusitools/ZusiTools.pas
+++ lib-zusitools/ZusiTools.pas
@@ -6,6 +6,7 @@ Uses Windows, Registry;
 
 function GetZusiDir: String;
 function IsZusiLoaded: Boolean;
+function SendKeyToZusi(Key: Integer; Pressed, Release: Boolean): Boolean;
 
 implementation
 
@@ -31,6 +32,26 @@ begin
   Wnd := FindWindow('TFormZusiD3DApplication',nil);
   Result := not (Wnd = 0);
 end;
+
+function SendKeyToZusi(Key: Integer; Pressed, Release: Boolean): Boolean;
+var
+  Wnd: HWND;
+begin
+  Wnd := FindWindow('TFormZusiD3DApplication',nil);
+
+  SetForegroundWindow(Wnd);
+  If Pressed then
+    keybd_event(Key,0,0,(Key shl 8) + 1) else
+      keybd_event(Key,0,KEYEVENTF_KEYUP,(Key shl 8) + 1);
+  If (Pressed and Release) then
+  begin
+    Sleep(10);
+    keybd_event(Key,0,KEYEVENTF_KEYUP,(Key shl 8) + 1);
+  end;
+  
+  Result := not (Wnd = 0);
+end;
+
 
 end.
  
