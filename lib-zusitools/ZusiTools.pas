e8c8d827859deeac79284d3c5275f10927c2fcc8
danny243 <danny243>
2005-03-16 15:17:09 +0000

    FindFiles hinzugefügt

--- lib-zusitools/ZusiTools.pas
+++ lib-zusitools/ZusiTools.pas
@@ -8,8 +8,57 @@ function GetZusiDir: String;
 function IsZusiLoaded: Boolean;
 function SendKeyToZusi(Key: Integer; Pressed, Release: Boolean): Boolean;
 function ShutdownComputer(Force: Boolean): Boolean;
+procedure FindFiles (aPath, aFindMask: String; aWithSub: Boolean; aResult: tStrings);
+
 
 implementation
+
+procedure FindFiles (aPath, aFindMask: String; aWithSub: Boolean; aResult: tStrings);
+var
+  FindRec: tSearchRec;
+begin
+  // Wenn die Stringliste nil ist oder aPath oder aFind nicht angegeben ist  
+  // dann raus  
+  If (aPath = '') or (aFindMask = '') or Not Assigned (aResult) Then  
+    Exit;  
+
+ 
+  // Wenn am Ende der Pfadangabe noch kein \ steht, dieses hinzufügen  
+  If aPath[Length (aPath)] <> '\' Then  
+    aPath := aPath + '\';  
+
+ 
+  // Im aktuellen Verzeichnis nach der Datei suchen  
+  If FindFirst (aPath + aFindMask, faAnyFile, FindRec) = 0 Then  
+    Repeat  
+      If (FindRec.Name <> '.') and (FindRec.Name <> '..') Then  
+        // ...Ergebnis in die Stringlist einfügen  
+        aResult.Add (aPath + FindRec.Name);  
+    Until FindNext (FindRec) <> 0;
+
+ 
+  FindClose (FindRec);  
+
+ 
+  // Wenn nicht in Unterverzeichnissen gesucht werden soll dann raus  
+  If Not aWithSub Then  
+    Exit;  
+
+ 
+  // In Unterverzeichnissen weiter suchen  
+  If FindFirst (aPath + '*.*', faAnyFile, FindRec) = 0 Then  
+    Repeat  
+      If (FindRec.Name <> '.') and (FindRec.Name <> '..') Then  
+        // Feststellen, ob es sich um ein Verzeichnis handelt  
+        If Boolean (FindRec.Attr and faDirectory) Then  
+          // Funktion erneut aufrufen, um Verzeichnis zu durchsuchen (Rekursion)  
+          FindFiles (aPath + FindRec.Name, aFindMask, aWithSub, aResult);  
+    Until FindNext (FindRec) <> 0;  
+
+ 
+   FindClose (FindRec);  
+end; 
+
 
 function GetZusiDir: String;
 var
