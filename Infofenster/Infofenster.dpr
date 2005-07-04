769bc58caa081cbcef420ecb975dc949f03876da
danny243 <danny243>
2005-07-04 13:58:10 +0000

    Möglichkeit, auch den Buchfahrplan und Zusi zu verschieben

--- Infofenster/Infofenster.dpr
+++ Infofenster/Infofenster.dpr
@@ -9,17 +9,32 @@ uses
 var
   WndInfo: HWND;
   WndZusi: HWND;
+  WndBfpl: HWND;
+
   Rect: TRect;
+
   InfoWndSize: TPoint;
   InfoWndPos: TPoint;
+
+  ZusiWndSize: TPoint;
+  ZusiWndPos: TPoint;
+
+  BfplWndSize: TPoint;
+  BfplWndPos: TPoint;
+
   ZusiPath: String;
   ZusiExe: String;
   SleepTime: Integer;
 
+resourcestring
+  sClassNameZusi = 'TFormZusiD3DApplication';
+  sClassNameInfo = 'TFormFahrtInfo';
+  sClassNameBfpl = 'TFormBuchfahrplan';
+
 begin
   OpenIni;
   ZusiPath := GetZusiFileName;
-  WndZusi := FindWindow('TFormZusiD3DApplication',nil);
+  WndZusi := FindWindow(PChar(sClassNameZusi), nil);
   Ini.Section := 'Settings';
   ZusiExe := Ini.ValueString('ZusiExe','Zusi.exe');
   If not FileExists(ZusiPath + ZusiExe) then
@@ -36,14 +51,18 @@ begin
 
   SleepTime := Ini.ValueInteger('Sleep',1000);
 
-  WndInfo := FindWindow('TFormFahrtInfo',nil);
+  WndInfo := FindWindow(PChar(sClassNameInfo), nil);
+  WndZusi := FindWindow(PChar(sClassNameZusi), nil);
+  WndBfpl := FindWindow(PChar(sClassNameBfpl), nil);
+
   If WndInfo = 0 then
   begin
     SleepEx(SleepTime, True);
     Repeat
-      WndInfo := FindWindow('TFormFahrtInfo',nil);
-      WndZusi := FindWindow('TFormZusiD3DApplication',nil);
-    Until (WndInfo <> 0) or (WndZusi = 0);
+      WndInfo := FindWindow(PChar(sClassNameInfo) ,nil);
+      WndZusi := FindWindow(PChar(sClassNameZusi) ,nil);
+      WndBfpl := FindWindow(PChar(sClassNameBfpl) ,nil);
+    Until ((WndInfo <> 0) and (WndBfpl <> 0)) or (WndZusi = 0);
   end;
   GetWindowRect(WndInfo, Rect);
 
@@ -56,4 +75,25 @@ begin
   SetWindowPos(WndInfo, 0, InfoWndPos.X, InfoWndPos.Y,
     InfoWndSize.X, InfoWndSize.Y, 0);
   SetWindowText(WndInfo, 'Info');
+
+  Ini.Section := 'Zusi';
+  GetWindowRect(WndZusi, Rect);
+  ZusiWndSize.X := Ini.ValueInteger('Width', Rect.Right - Rect.Left);
+  ZusiWndSize.Y := Ini.ValueInteger('Height', Rect.Bottom - Rect.Top);
+  ZusiWndPos.X := Ini.ValueInteger('PosX', Rect.Left);
+  ZusiWndPos.Y := Ini.ValueInteger('PosY', Rect.Top);
+  SetWindowPos(WndZusi, 0, ZusiWndPos.X, ZusiWndPos.Y,
+    ZusiWndSize.X, ZusiWndSize.Y, 0);
+
+  Ini.Section := 'Bfpl';
+  GetWindowRect(WndBfpl, Rect);
+  BfplWndSize.X := Ini.ValueInteger('Width', Rect.Right - Rect.Left);
+  BfplWndSize.Y := Ini.ValueInteger('Height', Rect.Bottom - Rect.Top);
+  BfplWndPos.X := Ini.ValueInteger('PosX', Rect.Left);
+  BfplWndPos.Y := Ini.ValueInteger('PosY', Rect.Top);
+  SetWindowPos(WndBfpl, 0, BfplWndPos.X, BfplWndPos.Y,
+    BfplWndSize.X, BfplWndSize.Y, 0);
+
+
+
 end.
